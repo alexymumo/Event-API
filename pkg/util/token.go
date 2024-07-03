6b5571295149c266d1e640d0cbe496110538480a
Alex Mumo <alexymumo2000@gmail.com>
2024-07-03 20:27:08 +0300

    minor fix

--- pkg/util/token.go
+++ pkg/util/token.go
@@ -9,7 +9,7 @@ import (
 
 var jwtSecret = []byte("SECRET_KEY")
 
-func createToken(username string) (string, error) {
+func CreateToken(username string) (string, error) {
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
 		"username": username,
 		"exp":      time.Now().Add(time.Hour * 24).Unix(),
@@ -21,7 +21,7 @@ func createToken(username string) (string, error) {
 	return tokenString, nil
 }
 
-func verifyToken(tokenString string) error {
+func VerifyToken(tokenString string) error {
 	token, err := jwt.Parse(tokenString, func(t *jwt.Token) (interface{}, error) {
 		return jwtSecret, nil
 	})
